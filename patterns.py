311dc4157b2e17245cb3a5ab0551da5e3a232a18
jatinmulani <jatinmulani409@gmail.com>
2026-03-19 11:32:17 +0530

    patterns.py

---
+++ patterns.py
@@ -0,0 +1,352 @@
+# # for  i in range(1,5):
+# #     for j in range(1,i+1):
+# #         print('*',end=' ')
+# #     print()
+# # for i in range(1,5):
+# #     for j in range(1,5-i+1):
+# #         print('*',end="")
+# #     print()
+
+# # for i in range(1,5):
+# #     for j in range(1,i+1):
+# #         print('*',end="")
+# #     print()
+
+# # for  i in range(1,5):
+# #     for j in range(1,5):
+# #         print(j,end="")
+# #     print('')
+
+# # for  i in range(1,5):
+# #     for j in range(1,5):
+# #         print(i,end="")
+# #     print('')
+
+# # for  i in range(1,5):
+# #     a=4
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a-=1
+# #     print('')
+
+# # for  i in range(1,5):
+# #     a=1
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a+=2
+# #     print('')
+
+# # for  i in range(1,5):
+# #     a=7
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a-=2
+# #     print('')
+
+
+# # for  i in range(1,5):
+# #     a=2
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a+=2
+# #     print('')
+
+
+# # for  i in range(1,5):
+# #     a=8
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a-=2
+# #     print('')
+# # a=1
+# # for  i in range(1,5):
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a+=1
+# #     print('')
+
+# # a=16
+# # for  i in range(1,5):
+# #     for j in range(1,5):
+# #         print(a,end="")
+# #         a-=1
+# #     print('')
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     print('')
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(i,end='')
+#     print('')
+
+# a=4
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(a,end='')
+#     a-=1
+#     print('')
+
+
+# a=1
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(a,end='')
+#         a+=1
+#     print('')
+
+# a=1
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(a,end='')
+#         a+=2
+#     print('')
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(j,end='')
+#     print('')
+
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(i,end='')
+#     print('')
+
+# n=4
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+
+#           print(n,end='')
+#     n-=1
+#     print('')
+
+# a=1
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a+=1
+#     print()
+# a=10
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a-=1
+#     print()
+# a=1
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a+=2
+#     print()
+
+# a=2
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a+=2
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(i,0,-1):
+#         print(k,end='')
+#     print()
+
+#     1
+#    21
+#   231
+#  2341
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print(i,end='')
+#     print()
+# a=4
+# for i in range(1,5):
+    
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print(a,end='')
+#     a-=1
+#     print()
+
+# a=1
+# for i in range(1,5):
+    
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print(a,end='')
+#         a+=1
+#     print()
+
+# for i in range(1,5):
+#     a=5-i
+#     for k in range(1,i+1):
+#         print(' ',end='')
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a-=1
+#     print()
+
+# for i in range(1,5):
+#     a=4
+#     for k in range(1,i+1):
+#         print(' ',end='')
+#     for j in range(1,5-i+1):
+#         print(i,end='')
+#         a-=1
+#     print()
+# a=4
+# for i in range(1,5):
+    
+#     for k in range(1,i+1):
+#         print(' ',end='')
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#     a-=1
+#     print()
+
+# a=1
+# for i in range(1,5):
+    
+#     for k in range(1,i+1):
+#         print(' ',end='')
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a+=1
+#     print()
+
+
+# a=1
+# for i in range(1,5):
+    
+#     for k in range(1,i+1):
+#         print(' ',end='')
+#     for j in range(1,5-i+1):
+#         print(a,end='')
+#         a+=2
+#     print()
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print('*',end='')
+      
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(' ',end='')
+#     for k in range(1,5-i+1):
+#         print('*',end='')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print('*',end=' ')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(' ',end='')
+#     for k in range(1,5-i+1):
+#         print('*',end=' ')
+#     print()
+# a=1
+# for i in range(1,5):
+    
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print(a+k-1,end='')
+#     a+=1
+#     print()
+# n=1
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     n=i
+#     for k in range(1,i+1):
+#         print(n,end='')
+#         n+=1
+#     n=n-2
+#     for k in range(2,i+1):
+#         print(n,end='')
+#         n-=1
+#     print()
+#       1
+#      232
+#     34543
+#    4567654
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(chr(96+i),end='')
+#     print()
+# n=9
+# for  i in range(1,9):
+  
+#     for j in range(1,9-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print(n+k-1,end='')
+   
+#     for k in range(i-1,0,-1):
+#         print(n+k-1,end='')
+#     n-=1
+#     print()
+# x=4
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(' ',end='')
+#     for k in range(1,5-i+1):
+#         if(k==1 or i==1 or k==x):
+#             print('*',end=' ')
+            
+#         else:
+#             print(' ',end=' ')
+#     x-=1
+#     print()
+# x=5
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         if(k==1 or i==k):
+#             print('*',end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#             print(' ',end='')
+#     for k in range(1,5-i+1):
+#             if(k==1 or k==5-i):
+#                 print('*',end=' ')
+#             else:
+#                 print(' ',end=' ')
+          
+#     print()
